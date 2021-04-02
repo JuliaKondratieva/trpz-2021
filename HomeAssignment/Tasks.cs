87c01eba1f5723bf4d2c6426fd136f9734c92482
Julia <juli4ka.kondratyeva@gmail.com>
2021-04-02 22:34:13 +0300

    Unit tests for functions

---
+++ HomeAssignment/Tasks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HomeAssignment
+{
+    public class Tasks
+    {
+        public static List<int> Task1Function(List<object> input)
+        {
+            
+            for (int i = input.Count; i >= 0; --i)
+            {
+                if(input[i] is string)
+                {
+                    input.RemoveAt(i);
+                }
+            }
+            var output = input.Cast<int>().ToList();
+            return output;
+
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Linq;
+using HomeAssignment;
+
+
+namespace Tests
+{
+    public class Tests
+    {
+
+        [TestCase(1,"a",1)]
+        public void Test1(int i1, string s1, int i2)
+        {
+            var input = new List<object>();
+            input.Add(i1);
+            input.Add(s1);
+            var output = new List<int>();
+            input.Add(i2);
+            // var input = new List<object>() { 1, 2, "a", "b" };
+
+            // var output = new List<int>() { 1, 2 };        
+
+
+            //CollectionAssert.AreEqual(expected, actual);
+            var result = Tasks.Task1Function(input);
+            CollectionAssert.AreEqual(output, result, "Returned result is incorrect");
+        }
+
+    }
+}
